7f6ba789febff8fdd5b0e58f948d70dd6e88c75b
MarianVizitiu <marin.vizzitiu@gmail.com>
2020-10-13 20:46:25 +0300

    added new line of comment locally not push

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Still really MY Coworker added awesome hello"); ///We are simulatiomadded comment//.....
+
+        //adding some awesome change
     }
 }
