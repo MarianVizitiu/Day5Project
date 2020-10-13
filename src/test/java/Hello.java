e52de552e780c0dd1102653200adf8234d5e5a92
MarianVizitiu <72089346+MarianVizitiu@users.noreply.github.com>
2020-10-13 20:47:22 +0300

    co-worker added more change her

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Still really MY Coworker added awesome hello"); ///We are simulatiomadded comment//.....
+        //co-worker pushed changes that
     }
 }
