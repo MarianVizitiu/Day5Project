938d572b0eb563ca989c4147b0003713282e6da4
MarianVizitiu <72089346+MarianVizitiu@users.noreply.github.com>
2020-10-13 20:39:58 +0300

    added more changes by co-worker

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Still really awesome hello"); ///added comment
+        System.out.println("Still really MY Coworker added awesome hello"); ///We are simulatiomadded comment
     }
 }
