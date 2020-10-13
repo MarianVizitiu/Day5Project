310bd69da437e6bb521b1c9f3740622a26d780b6
MarianVizitiu <marin.vizzitiu@gmail.com>
2020-10-13 20:41:44 +0300

    added comment on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Still really awesome hello"); ///added comment
+        System.out.println("Still really awesome hello"); ///added comment//This is my local hello, and commit
+        //This is my own hello
     }
 }
